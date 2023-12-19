454bb9bbd597df6a6a6293ee1eb6d7c044cc4af2
maguitaria <t2glma00@students.oamk.fi>
2023-12-19 14:47:25 +0200

    Add integration test

--- tests/mainpage.spec.ts
+++ tests/mainpage.spec.ts
@@ -1,32 +1,36 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
   
-    await page.goto('http://localhost:5173/weather-app-react/');
-    await page.locator('div').filter({ hasText: /^Weather in Oulu, Finland$/ }).nth(2).click();
-    await expect (page.getByRole('button', { name: 'Icon' })).toBeVisible();
-    await page.getByRole('button', { name: 'Icon' }).click();
-    await expect (page.getByRole('heading', { name: 'Monday' })).toBeVisible();
 
-    await page.locator('.flex-auto > div > div').first().click();
-    await page.getByText('Wind speed:').click();
-    await page.getByText('UV Index:').click();
-    await page.getByText('Precipitation:').click();
-    await page.getByText('Sunrise:').click();
-    await page.getByText('Sunset:').click();
-    
-    await page.locator('div:nth-child(3) > .w-full').click();
-    await page.getByRole('button', { name: 'weather icon' }).click();
-    await page.getByText('Tuesday').click();
-    await page.getByRole('button', { name: 'weather icon' }).click();
-    await page.getByText('Wednesday').click();
-    await page.getByRole('button', { name: 'weather icon' }).click();
-    await page.getByText('Thursday').click();
-    await page.getByRole('button', { name: 'weather icon' }).click();
-    await page.getByText('Friday').click();
-    await page.getByRole('button', { name: 'weather icon' }).click();
-    await page.getByText('Saturday').click();
-    await page.getByRole('button', { name: 'weather icon' }).click();
-    await page.getByText('Sunday').click();
-    await page.getByRole('button', { name: 'weather icon' }).click();
-});
+
+    test('Go through elements and click on 7 days cards', async ({ page }) => {
+        await page.goto('http://localhost:5173/weather-app-react/');
+        await page.waitForLoadState()
+        await page.locator('div').filter({ hasText: /^Weather in Oulu, Finland$/ }).nth(2).click();
+        await page.getByText('Current WeatherSnow').click();
+        await page.locator('.w-20').click(); // Click on weather icon
+
+        await page.getByText('Today').click(); // Click on first card
+
+        await page.locator('.flex-auto > div > div').first().click(); // Flex element should be visible
+        await page.getByText('Wind:').click();
+        await page.getByText('UV Index:').click();
+        await page.getByText('Precipitation:').click();
+        await page.getByText('Sunrise:').click();
+        await page.getByText('Sunset:').click();
+        // Click on weather icons one by one on 7 cards
+        await page.locator('div:nth-child(2) > .bg-light-blue > .w-40').click();
+        await page.locator('div:nth-child(3) > .bg-light-blue > .w-40').click();
+        await page.locator('div:nth-child(4) > .bg-light-blue > .w-40').click();
+        await page.locator('div:nth-child(5) > .bg-light-blue > .w-40').click();
+        await page.locator('div:nth-child(6) > .bg-light-blue > .w-40').click();
+        await page.locator('div:nth-child(7) > .bg-light-blue > .w-40').click();
+        // Go to the chart
+        await page.getByRole('heading', { name: 'Hourly chart' }).click();
+        await page.locator('canvas').click({
+            position: {
+                x: 266,
+                y: 4
+            }
+        });
+    });
